5aa31ccf8fc5aeae7e9f37749d41f3ecc22c0e14
shawshankkumar <shashankkumarthakur@gmail.com>
2023-06-10 18:44:43 +0530

    chore: Add basic middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
+const helmet =require('helmet');
+const rateLimit = require('express-rate-limit')
+const cors = require('cors');
 
+// @ts-ignore
+const limiter = rateLimit({
+	windowMs: 60 * 1000, 
+	max: 100, 
+	standardHeaders: true, 
+    message: 'Too many requests, please try again later.',
+    })
+
+app.use(limiter)
 app.use(express.json());
+app.use('/', helmet());
+app.use('/', cors());
+app.use(express.urlencoded({ extended: true }));
 
 //Route Imports
 const product = require('./routes/productRoute');
